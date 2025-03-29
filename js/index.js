c89eaa8da87251a322bae9df8bc6b138e47359ea
AylinSantaCruzV <79342803+AnthuA25@users.noreply.github.com>
2025-03-29 15:30:50 +0000

    feat:Add styles in js

--- js/index.js
+++ js/index.js
@@ -27,11 +27,21 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault();
     const message = document.createElement("div");
     message.innerHTML=`<p>Mensaje enviado con exito</p>`;
+    message.style.background = '#d30068';
+    message.style.padding = "10px";
+    message.style.borderRadius = "5px";
+    message.style.color = "white"
+    message.style.marginTop="8px"
     container.appendChild(message);
     document.getElementById("form-contact").reset();
   }
 
   form.addEventListener("submit", sendForm);
+
+  /* Filters category*/
+  const containerProduct = document.querySelector(".filter-prodcuts");
+  const filter = document.querySelector()
+  
 
 
 });
