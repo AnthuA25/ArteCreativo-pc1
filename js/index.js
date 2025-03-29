c6e10c2a5063e6fe298bf50295e7dfc37f7b4513
AylinSantaCruzV <79342803+AnthuA25@users.noreply.github.com>
2025-03-29 14:45:57 +0000

    feat:Implement form submission

--- js/index.js
+++ js/index.js
@@ -18,4 +18,20 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.remove("active");
     overlay.classList.remove("active");
   });
+
+  /*Contact*/
+  const form = document.getElementById("form-contact");
+  const container = document.querySelector('.form');
+
+  const sendForm = (event) =>{
+    event.preventDefault();
+    const message = document.createElement("div");
+    message.innerHTML=`<p>Mensaje enviado con exito</p>`;
+    container.appendChild(message);
+    document.getElementById("form-contact").reset();
+  }
+
+  form.addEventListener("submit", sendForm);
+
+
 });
